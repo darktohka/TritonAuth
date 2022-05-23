e4c8b2365921843870c6fa34879493c705d4d91a
Disyer <daniel@tohka.us>
2022-05-23 18:32:54 +0300

    login: Overhaul login widget

--- tritonauth/LoginWidget.py
+++ tritonauth/LoginWidget.py
@@ -15,7 +15,14 @@ class LoginWidget(TritonWidget):
         self.widget.setContentsMargins(20, 20, 20, 20)
 
         self.boxLayout = QVBoxLayout()
-        self.label = QLabel('Password')
+
+        font = QFont('Helvetica', 13)
+        font.setBold(True)
+
+        self.title = QLabel('Unlock TritonAuth Database')
+        self.title.setFont(font)
+
+        self.label = QLabel('Enter Password:')
         self.label.setFont(QFont('Helvetica', 10))
 
         self.passwordWidget = QWidget()
@@ -29,11 +36,11 @@ class LoginWidget(TritonWidget):
         self.passBox.returnPressed.connect(self.handleLogin)
         self.setBackgroundColor(self.passBox, QColor(238, 238, 238))
 
-        self.okButton = QPushButton('OK')
-        self.okButton.clicked.connect(self.handleLogin)
+        self.unlockButton = QPushButton('Unlock')
+        self.unlockButton.clicked.connect(self.handleLogin)
 
         self.passLayout.addWidget(self.passBox)
-        self.passLayout.addWidget(self.okButton)
+        self.passLayout.addWidget(self.unlockButton)
         self.passwordWidget.setLayout(self.passLayout)
 
         self.wrongLabel = QLabel()
@@ -42,6 +49,8 @@ class LoginWidget(TritonWidget):
         palette.setColor(QPalette.WindowText, QColor(255, 0, 0))
         self.wrongLabel.setPalette(palette)
 
+        self.boxLayout.addWidget(self.title)
+        self.boxLayout.addSpacing(10)
         self.boxLayout.addWidget(self.label)
         self.boxLayout.addWidget(self.passwordWidget)
         self.boxLayout.addWidget(self.wrongLabel)
