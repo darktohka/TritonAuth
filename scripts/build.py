4778ee4fb1c9920169201f73fc6a423c5d0b1c59
Disyer <daniel@tohka.us>
2022-05-23 23:29:41 +0300

    build: Enable numpy

--- scripts/build.py
+++ scripts/build.py
@@ -17,7 +17,7 @@ def main():
     build_cmd = [
         sys.executable, '-OO', '-m', 'nuitka', '--standalone', '--onefile', '--python-flag=-OO', '--assume-yes-for-downloads',
         '--static-libpython=auto', '--windows-disable-console', '--windows-icon-from-ico=scripts/icon.ico', '--windows-product-name=TritonAuth',
-        '--windows-company-name=Sapientia', '--windows-file-version=1.0.0.0', '--enable-plugin=pyside6', '--noinclude-custom-mode=numpy:nofollow',
+        '--windows-company-name=Sapientia', '--windows-file-version=1.0.0.0', '--enable-plugin=pyside6', '--enable-plugin=numpy',
         '-o', 'TritonAuth.exe', 'main.py'
     ]
     result = subprocess.Popen(build_cmd, shell=True, cwd='..')
